9eae96bfb726be7409f572278a1d4ed5ac9d6314
jeon1322 <88426334+jeon1322@users.noreply.github.com>
2022-07-10 16:24:52 +0900

    2022/07/10 정준영 commit

--- doggentle/src/main/java/com/gentle/www/controller/GDetail.java
+++ doggentle/src/main/java/com/gentle/www/controller/GDetail.java
@@ -1,31 +1,7 @@
 package com.gentle.www.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.gentle.www.dao.GDetailDao;
-import com.gentle.www.vo.GDetailVO;
-
-
-	@Controller
-	@RequestMapping("/gDetail")
-	public class GDetail {
+public class GDetail {
 		
-		@Autowired
-		GDetailDao gDao;
-
-		
-		@RequestMapping("/gDetail.dog")
-		public ModelAndView GoodsDetail(ModelAndView mv, GDetailVO gVO) {
-			GDetailVO gno = gDao.gDetInfo(gVO.getGno());
-			mv.addObject("DATA", gVO);
-			mv.setViewName("gDetail/gDetail");
-			
-			return mv;
-		}
 
 }
--- doggentle/src/main/java/com/gentle/www/controller/MainController.java
+++ doggentle/src/main/java/com/gentle/www/controller/MainController.java
@@ -15,15 +15,9 @@ import com.gentle.www.vo.GDetailVO;
 
 @Controller
 public class MainController {
-	@Autowired
-	GDetailDao gDao;
 	
 	@RequestMapping({"/", "/main.dog"})
 	public ModelAndView getMain(ModelAndView mv, HttpSession session) {
-		List<GDetailVO> list = gDao.getStratCate();
-		
-		//데이터 심고
-		session.setAttribute("LIST", list);
 		mv.setViewName("main");
 		return mv;
 	}
@@ -33,17 +27,5 @@ public class MainController {
 		return "error";
 	}
 	
-	@RequestMapping("/cateList.dog")
-	@ResponseBody
-	public List<GDetailVO> cateList(GDetailVO gVO){
-		List<GDetailVO> list = gDao.getCateList(gVO);
-		return list;
-	}
 	
-	// 상품 리스트 요청 전담 처리함수
-	@RequestMapping("/productList.dog")
-	@ResponseBody
-	public List<GDetailVO> productList(GDetailVO gVO){
-		return gDao.getProductList(gVO);
-	}
 }

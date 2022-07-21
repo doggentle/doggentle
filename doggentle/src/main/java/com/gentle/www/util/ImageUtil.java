32a0b55e0a20ec4a40d67ca1b99b8fef07434da3
tmddus2123 <tmddus2123@gmail.com>
2022-07-21 22:20:58 +0900

    22.07.21

--- doggentle/src/main/java/com/gentle/www/util/ImageUtil.java
+++ doggentle/src/main/java/com/gentle/www/util/ImageUtil.java
@@ -10,23 +10,23 @@ public class ImageUtil {
 			같은 이름의 파일이 존재하면
 			"_숫자" 를 붙여서 이름을 바꾸는 방식
 		*/
-
+		
 		int count = 0;
-
+		
 		String newName = upName;
-
-
+		
+		
 		File file = new File(path, newName);
-
+		
 		while(file.exists()) { // 같은 이름의 이미지가 존재하는 경우
 			count++;
-
+			
 			int len = upName.lastIndexOf(".");
 			String preName = upName.substring(0, len);
 			String ext = upName.substring(len);
-
+			
 			newName = preName + "_" + count + ext;
-
+			
 			file = new File(path, newName);
 		}
 		return newName;
